91dfa715e42faddec969b70e72732779e42b9a03
Exdra <108461934+Exdra@users.noreply.github.com>
2022-09-12 23:30:08 +0200

    Delete Build.sh

--- Build.sh
+++
@@ -1,8 +0,0 @@
-# Raylib linux build command by hisham "Exdra"
-echo "Enter The file name : "
-read File_name
-
-echo "Enter the Excutble name : "
-read Excutatble_name
-
-gcc $File_name.c -o $Excutatble_name.out -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
